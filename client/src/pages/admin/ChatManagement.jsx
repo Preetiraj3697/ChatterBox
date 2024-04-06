13d90eb1dd36618867b91cd1b8f43994479b771c
Preeti Raj <satendra11kum@gmail.com>
2024-04-06 18:23:14 +0530

    Frontend

--- client/src/pages/admin/ChatManagement.jsx
+++ client/src/pages/admin/ChatManagement.jsx
@@ -1,12 +1,93 @@
-import React from 'react'
-import AdminLayout from '../../components/layout/AdminLayout'
+import { useFetchData } from "6pp";
+import { Avatar, Skeleton } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import AdminLayout from "../../components/layout/AdminLayout";
+import Table from "../../components/shared/Table";
+import { transformImage } from "../../lib/features";
+import { dashboardData } from "../../components/constants/sampleData";
+const columns = [
+  {
+    field: "id",
+    headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    headerClassName: "table-header",
+    width: 150,
+    renderCell: (params) => <AvatarCard avatar={params.row.avatar} />,
+  },
 
+  {
+    field: "name",
+    headerName: "Name",
+    headerClassName: "table-header",
+    width: 300,
+  },
+
+  {
+    field: "groupChat",
+    headerName: "Group",
+    headerClassName: "table-header",
+    width: 100,
+  },
+  {
+    field: "totalMembers",
+    headerName: "Total Members",
+    headerClassName: "table-header",
+    width: 120,
+  },
+  {
+    field: "members",
+    headerName: "Members",
+    headerClassName: "table-header",
+    width: 400,
+    renderCell: (params) => (
+      <AvatarCard max={100} avatar={params.row.members} />
+    ),
+  },
+  {
+    field: "totalMessages",
+    headerName: "Total Messages",
+    headerClassName: "table-header",
+    width: 120,
+  },
+  {
+    field: "creator",
+    headerName: "Created By",
+    headerClassName: "table-header",
+    width: 250,
+    renderCell: (params) => (
+      <Stack direction="row" alignItems="center" spacing={"1rem"}>
+        <Avatar alt={params.row.creator.name} src={params.row.creator.avatar} />
+        <span>{params.row.creator.name}</span>
+      </Stack>
+    ),
+  },
+];
 const ChatManagement = () => {
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    setRows(
+      dashboardData.chats.map((i) => ({
+        ...i,
+        id: i._id,
+        avatar: i.avatar.map((i) => transformImage(i, 50)),
+        members: i.members.map((i) => transformImage(i.avatar, 50)),
+        creator: {
+          name: i.creator.name,
+          avatar:transformImage(i.creator.avatar,50)
+        }
+      }))
+    );
+  }, []);
   return (
-      <AdminLayout>
-          <div>ChatManagement</div>
+    <AdminLayout>
+      <Table heading={"All users"} columns={columns} rows={rows} />
     </AdminLayout>
-  )
-}
+  );
+};
 
-export default ChatManagement
+export default ChatManagement;
--- client/src/pages/admin/UserManagement.jsx
+++ client/src/pages/admin/UserManagement.jsx
@@ -1,10 +1,66 @@
-import React from "react";
+import { useFetchData } from "6pp";
+import { Avatar, Skeleton } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
+import Table from "../../components/shared/Table";
+import { transformImage } from "../../lib/features";
+import { dashboardData } from "../../components/constants/sampleData";
+const columns = [
+  {
+    field: "id",
+    headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    headerClassName: "table-header",
+    width: 150,
+    renderCell: (params) => (
+      <Avatar alt={params.row.name} src={params.row.avatar} />
+    ),
+  },
 
+  {
+    field: "name",
+    headerName: "Name",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "username",
+    headerName: "Username",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "friends",
+    headerName: "Friends",
+    headerClassName: "table-header",
+    width: 150,
+  },
+  {
+    field: "groups",
+    headerName: "Groups",
+    headerClassName: "table-header",
+    width: 200,
+  },
+];
 const UserManagement = () => {
+  const [rows, setRows] = useState([]);
+  useEffect(() => {
+    setRows(
+      dashboardData.users.map((i) => ({
+        ...i,
+        id: i._id,
+        avatar: transformImage(i.avatar,50),
+      }))
+    );
+  }, []);
   return (
     <AdminLayout>
-      <div>Usermanagement</div>
+      <Table heading={"All users"} columns={columns} rows={rows} />
     </AdminLayout>
   );
 };
