e515f72fed6bc367b2627031b6a764767972f257
Preeti Raj <satendra11kum@gmail.com>
2024-04-06 17:40:21 +0530

    Charts

--- client/src/lib/features.js
+++ client/src/lib/features.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 const fileFormat = (url = "") => {
     const fileExt = url.split(".").pop();
     
@@ -13,5 +14,36 @@ const fileFormat = (url = "") => {
 
   return "file";
 };
-const transformImage = (url = "", width=100) => url;
-export { fileFormat,transformImage };
+// https://res.cloudinary.com/dj5q966nb/image/upload/dpr_auto/w_200/v1710344436/fafceddc-2845-4ae7-a25a-632f01922b4d.png
+
+// /dpr_auto/w_200
+const transformImage = (url = "", width = 100) => {
+  const newUrl = url.replace("upload/", `upload/dpr_auto/w_${width}/`);
+
+  return newUrl;
+};
+
+const getLast7Days = () => {
+  const currentDate = moment();
+
+  const last7Days = [];
+
+  for (let i = 0; i < 7; i++) {
+    const dayDate = currentDate.clone().subtract(i, "days");
+    const dayName = dayDate.format("dddd");
+
+    last7Days.unshift(dayName);
+  }
+
+  return last7Days;
+};
+
+const getOrSaveFromStorage = ({ key, value, get }) => {
+  if (get)
+    return localStorage.getItem(key)
+      ? JSON.parse(localStorage.getItem(key))
+      : null;
+  else localStorage.setItem(key, JSON.stringify(value));
+};
+
+export { fileFormat, transformImage, getLast7Days, getOrSaveFromStorage };
