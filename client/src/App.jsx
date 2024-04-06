f7fc5dadd1a3cf54f29e5dd40c7faa3ba703414f
Preeti Raj <satendra11kum@gmail.com>
2024-04-06 16:06:42 +0530

    adminSidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,15 @@ import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
 import { LayoutLoader } from "./components/layout/Loaders";
+
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
 const NotFound = lazy(() => import("./pages/NotFoud"));
+const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
+
 let user = true;
 
 const App = () => {
@@ -21,7 +25,9 @@ const App = () => {
         </Route>
         <Route path="/login" element={<ProtectRoute user={!user} redirect="/">
           <Login />
-        </ProtectRoute>} />
+          </ProtectRoute>} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       </Suspense>
